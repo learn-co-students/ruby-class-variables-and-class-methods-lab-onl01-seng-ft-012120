9e18e48afca239a502921152596e8b7d01276654
Kyle McKenna <mckennakyler@gmail.com>
2020-01-30 17:32:45 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -11,6 +11,9 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1 
+    # for artist and genre return a unique array for each of existing songs
+    @@artists << artist 
+    @@genres << genre 
   end
 
   # method count
@@ -32,15 +35,30 @@ class Song
   # method genre_count, returns a hash: keys are genre_count => value that is a count of the number of songs in the genre
   def self.genre_count
     genre_count = {} # new hash
-    genre_count.each {|genre| genre_count[genre] += 1} # value key set 
-    return genre_count
+    
+    #@@genres.each {|genre| genre_count[genre] += 1}  # value key set(code is currently wrong on this line) 
+    
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end   
+    genre_count
   end 
   
   #same as previous method, but for artist_count
   def self.artist_count
-    artist_count = {} 
-    artist_count.each {|artist| artist_count[artist] += 1} 
-    return artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
   end   
   
 end 
