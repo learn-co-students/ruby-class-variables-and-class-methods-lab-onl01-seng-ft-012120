402bc39fb51035294a5c914ce5d25877a9db8f92
Ola Enol <eniolar10@gmail.com>
2020-02-03 04:33:44 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song
+end
+
+Song.count
+Song.artists
+Song.genres
+Song.genre_count
+Song.artist_count
+ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
+
+ninety_nine_problems.name
+
+ninety_nine_problems.artist
+
+ninety_nine_problems.genre
+Song.genre_count
